24870128aaa2bcffe62bb322aa0e5ece8e9bf16c
TatianaKosovskaya <99216721+TatianaKosovskaya@users.noreply.github.com>
2023-03-16 01:11:07 +0300

    Update Untitled37.py

--- Untitled37.py
+++ Untitled37.py
@@ -13,5 +13,5 @@ import pandas as pd
 url='https://drive.google.com/file/d/1lnBoSa6wV5cb8k996hXhePGqtkEsxHbN/view?usp=share_link'
 path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
 df = pd.read_csv(path)
-df.head()
+print(df.head())
 
