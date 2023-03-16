5bbd9a3b74a191481544df0afcd85f4f773951df
TatianaKosovskaya <99216721+TatianaKosovskaya@users.noreply.github.com>
2023-03-16 17:15:22 +0300

    Update Untitled37.py

--- Untitled37.py
+++ Untitled37.py
@@ -10,8 +10,12 @@ import pandas as pd
 # In[7]:
 
 
-url='https://drive.google.com/file/d/1lnBoSa6wV5cb8k996hXhePGqtkEsxHbN/view?usp=share_link'
-path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-df = pd.read_csv(path)
-st.table(df.head())
+#url='https://drive.google.com/file/d/1lnBoSa6wV5cb8k996hXhePGqtkEsxHbN/view?usp=share_link'
+#path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
+#df = pd.read_csv(path)
+#st.table(df.head())
 
+#https://github.com/TatianaKosovskaya/diploma/blob/main/mushrooms.csv
+url='https://github.com/TatianaKosovskaya/diploma/blob/main/mushrooms.csv'
+df = pd.read_csv(url)
+st.table(df.head())
