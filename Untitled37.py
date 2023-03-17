adae9f7b8aea183369f97b6433e49d92b359559c
TatianaKosovskaya <99216721+TatianaKosovskaya@users.noreply.github.com>
2023-03-17 16:49:30 +0300

    Update Untitled37.py

--- Untitled37.py
+++ Untitled37.py
@@ -7,9 +7,10 @@ import streamlit as st
 import pandas as pd
 
 df_1 = pd.read_csv('train_df_1.csv')
-df_1.head()
 st.table(df_1.head())
 
 df_2 = pd.read_csv('train_df_2.csv')
-df_2.head()
 st.table(df_2.head())
+
+df_merged = df_1.append(df_2, ignore_index=True)
+st.table(df_merged.head())
