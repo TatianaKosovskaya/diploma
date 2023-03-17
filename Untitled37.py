50deb4cec888ef3dc86334ca79f13c440d7af704
TatianaKosovskaya <99216721+TatianaKosovskaya@users.noreply.github.com>
2023-03-17 16:44:52 +0300

    Update Untitled37.py

--- Untitled37.py
+++ Untitled37.py
@@ -5,8 +5,7 @@
 
 import streamlit as st
 import pandas as pd
-url='https://drive.google.com/file/d/1SnMOAOYZUJFR8hUhK58pOU3vLTRaRqvz/view?usp=share_link'
-path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
-df = pd.read_csv(path)
-df.head()
-st.table(df.head())
+
+df_1 = pd.read_csv('train_df_1.csv')
+df_1.head()
+st.table(df_1.head())
