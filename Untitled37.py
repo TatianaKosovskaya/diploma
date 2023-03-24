cc3f4f21e6c4b5509f1ad10aacdc550b9e171e49
TatianaKosovskaya <99216721+TatianaKosovskaya@users.noreply.github.com>
2023-03-24 16:00:11 +0300

    Update Untitled37.py

--- Untitled37.py
+++ Untitled37.py
@@ -9,8 +9,8 @@ import pandas as pd
 
 st.text('Hi!')
 
-#df_1 = pd.read_csv('train_df_1.csv')
-#st.table(df_1.head())
+df_1 = pd.read_csv('train_df_1.csv')
+st.table(df_1.head())
 
 #df_2 = pd.read_csv('train_df_2.csv')
 #st.table(df_2.head())
