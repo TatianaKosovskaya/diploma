266e9518a90afd318ae7bb8d7e8489d21a2ce25b
TatianaKosovskaya <99216721+TatianaKosovskaya@users.noreply.github.com>
2023-03-24 16:03:27 +0300

    Update Untitled37.py

--- Untitled37.py
+++ Untitled37.py
@@ -19,3 +19,14 @@ df_merged = df_1.append(df_2, ignore_index=True)
 st.table(df_merged.head())
 st.table(df_merged.tail())
 
+import glob
+import os
+# merging the files
+joined_files = os.path.join("train_df_*.csv")
+  
+# A list of all joined files is returned
+joined_list = glob.glob(joined_files)
+  
+# Finally, the files are joined
+df = pd.concat(map(pd.read_csv, joined_list), ignore_index=True)
+st.table(df.head())
