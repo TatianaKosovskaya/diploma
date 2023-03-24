f7b0da7189927846eff3e02f5af03fa0e0d8dacf
TatianaKosovskaya <99216721+TatianaKosovskaya@users.noreply.github.com>
2023-03-24 16:00:36 +0300

    Update Untitled37.py

--- Untitled37.py
+++ Untitled37.py
@@ -12,8 +12,8 @@ st.text('Hi!')
 df_1 = pd.read_csv('train_df_1.csv')
 st.table(df_1.head())
 
-#df_2 = pd.read_csv('train_df_2.csv')
-#st.table(df_2.head())
+df_2 = pd.read_csv('train_df_2.csv')
+st.table(df_2.head())
 
 #df_merged = df_1.append(df_2, ignore_index=True)
 #st.table(df_merged.head())
