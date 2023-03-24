b9e37a5caf9f04463b6feb00e480bf7b681f816e
TatianaKosovskaya <99216721+TatianaKosovskaya@users.noreply.github.com>
2023-03-24 15:52:24 +0300

    Update Untitled37.py

--- Untitled37.py
+++ Untitled37.py
@@ -7,13 +7,15 @@ import streamlit as st
 import pandas as pd
 #import os
 
-df_1 = pd.read_csv('train_df_1.csv')
-st.table(df_1.head())
+st.text('Hi!')
 
-df_2 = pd.read_csv('train_df_2.csv')
-st.table(df_2.head())
+#df_1 = pd.read_csv('train_df_1.csv')
+#st.table(df_1.head())
 
-df_merged = df_1.append(df_2, ignore_index=True)
-st.table(df_merged.head())
-st.table(df_merged.tail())
+#df_2 = pd.read_csv('train_df_2.csv')
+#st.table(df_2.head())
+
+#df_merged = df_1.append(df_2, ignore_index=True)
+#st.table(df_merged.head())
+#st.table(df_merged.tail())
 
