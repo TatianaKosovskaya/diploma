f06d3f5360080fb556fb12c7ca083c75d2a476f5
TatianaKosovskaya <99216721+TatianaKosovskaya@users.noreply.github.com>
2023-05-04 16:20:09 +0300

    Update version_1.py

--- version_1.py
+++ version_1.py
@@ -5,10 +5,12 @@ import pandas as pd
 st.text('Hi!')
 
 data_1 = pd.read_csv('train_df_1.csv')
+data_2 = pd.read_csv('train_df_2.csv')
 
 st.text('Hi 2!')
 
 st.table(data_1.head())
+st.table(data_2.head())
 
 st.text('Hi 3!')
 
