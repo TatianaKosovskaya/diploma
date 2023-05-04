a049f64903ca0b4501bdc4b2d890677ff426b774
TatianaKosovskaya <99216721+TatianaKosovskaya@users.noreply.github.com>
2023-05-04 14:35:04 +0300

    Update Анализ данных для ВКР 10.py

--- Анализ данных для ВКР 10.py
+++ Анализ данных для ВКР 10.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
+data_1 = pd.read_csv('train_df_1.csv')
+data_2 = pd.read_csv('train_df_2.csv')
+data_3 = pd.read_csv('train_df_3.csv')
+data_4 = pd.read_csv('train_df_4.csv')
+data_5 = pd.read_csv('train_df_5.csv')
+data_6 = pd.read_csv('train_df_6.csv')
+data_7 = pd.read_csv('train_df_7.csv')
+data_8 = pd.read_csv('train_df_8.csv')
+data_9 = pd.read_csv('train_df_9.csv')
+data_10 = pd.read_csv('train_df_10.csv')
+data = df_1.append(df_2, ignore_index=True)
+data = data.append(df_3, ignore_index=True)
+data = data.append(df_4, ignore_index=True)
+data = data.append(df_5, ignore_index=True)
+data = data.append(df_6, ignore_index=True)
+data = data.append(df_7, ignore_index=True)
+data = data.append(df_8, ignore_index=True)
+data = data.append(df_9, ignore_index=True)
+data = data.append(df_10, ignore_index=True)
+st.table(data)
 
 # In[2]:
 
