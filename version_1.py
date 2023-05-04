391e47f18ecee51e793682177df81de66e27b6dd
TatianaKosovskaya <99216721+TatianaKosovskaya@users.noreply.github.com>
2023-05-04 16:24:07 +0300

    Update version_1.py

--- version_1.py
+++ version_1.py
@@ -6,18 +6,21 @@ st.text('Hi!')
 
 data_1 = pd.read_csv('train_df_1.csv')
 data_2 = pd.read_csv('train_df_2.csv')
+data_3 = pd.read_csv('train_df_3.csv')
+data_4 = pd.read_csv('train_df_4.csv')
+data_5 = pd.read_csv('train_df_5.csv')
+data_6 = pd.read_csv('train_df_6.csv')
+data_7 = pd.read_csv('train_df_7.csv')
+data_8 = pd.read_csv('train_df_8.csv')
+data_9 = pd.read_csv('train_df_9.csv')
+data_10 = pd.read_csv('train_df_10.csv')
 
 st.text('Hi 2!')
 
-st.table(data_1.head())
-st.table(data_2.head())
+data = pd.concat([data_1, data_2, data_3, data_4, data_5, data_6, data_7, data_8, data_9, data_10], axis=0, ignore_index=True)
 
 st.text('Hi 3!')
 
-data = pd.concat([data_1, data_2], axis=0, ignore_index=True)
-
-st.text('Hi 4!')
-
 st.table(data.tail())
 
-st.text('Hi 5!')
+st.text('Hi 4!')
