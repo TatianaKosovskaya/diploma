8a21525c3ebc8ee5de45c2193f14fc557aee2aa0
TatianaKosovskaya <99216721+TatianaKosovskaya@users.noreply.github.com>
2023-05-09 11:29:12 +0300

    Update ui_for_users.py

--- ui_for_users.py
+++ ui_for_users.py
@@ -45,6 +45,7 @@ data_1 = pd.read_csv('train_df_1.csv')
 #data_8 = pd.read_csv('train_df_8.csv')
 #data_9 = pd.read_csv('train_df_9.csv')
 #data_10 = pd.read_csv('train_df_10.csv')
+#data = pd.concat([data_1, data_2, data_3, data_4, data_5, data_6, data_7, data_8, data_9, data_10], axis=0, ignore_index=True)
 
 data=data_1
 data.drop('Unnamed: 0', axis= 1 , inplace= True )
@@ -71,87 +72,91 @@ data.loc[-1] = ['9999999', id, '9999999', '9999999', '9999999', '9999999', '9999
 data.index = data.index + 1 
 data = data.sort_index()
 
-st.table(data.head())
+if click:
+    
+    with st.spinner('Due to the calculation of the machine learning model you have to wait a few seconds for the result.'):
 
-'''
-#data = pd.concat([data_1, data_2, data_3, data_4, data_5, data_6, data_7, data_8, data_9, data_10], axis=0, ignore_index=True)
+        if sum(arr1 + arr2) == len(product_list):
 
-data.rename( columns={'Derivada account':'Derivative account'}, inplace=True )
-l = list(data.columns)
-data_desc = pd.read_csv('data_desc.csv')
-data_desc['Column Name'] = l
-data_2 = data.copy()
-data_2 = data_2[data_2['Employee index'].notna()]
-data_2 = data_2[data_2['Country residence'].notna()]
-data_2 = data_2[data_2['Sex'].notna()]
-data_2 = data_2[data_2['Index'].notna()]
-data_2 = data_2[data_2['Index real'].notna()]
-data_2 = data_2[data_2['Residence index'].notna()]
-data_2 = data_2[data_2['Foreigner index'].notna()]
-data_2 = data_2[data_2['Deceased index'].notna()]
-data_2 = data_2[data_2['Province code'].notna()]
-data_2 = data_2[data_2['Province name'].notna()]
-data_2 = data_2[data_2['Activity index'].notna()]
-data_2 = data_2[data_2['Payroll'].notna()]
-data_2 = data_2[data_2['Real pensions'].notna()]
-data_2.drop(columns=['Last date as primary customer','Spouse index'], inplace=True)
-data_2['Gross income of the household'] = data_2['Gross income of the household'].fillna(134245.63)
-data_2['Customer type'] = data_2['Customer type'].fillna(1)
-data_2['Customer relation type'] = data_2['Customer relation type'].fillna('I')
-data_2['Channel index'] = data_2['Channel index'].fillna('KHE')
-data_2['Segmentation index'] = data_2['Segmentation index'].fillna('02 - PARTICULARES')
-data_2[['Age', 'Seniority (in months)']] = data_2[['Age', 'Seniority (in months)']].astype(int)
-data_2 = data_2[data_2["Age"] <= 100]
-data_2 = data_2[data_2["Age"] >= 18]
-data_2 = data_2[data_2["Seniority (in months)"] != -999999]
-# Исправление категорий столбца - indrel_1mes
-data_2['Customer type'].replace('1', 1, inplace=True)
-data_2['Customer type'].replace('1.0', 1, inplace=True)
-data_2['Customer type'].replace('2', 2, inplace=True)
-data_2['Customer type'].replace('2.0', 2, inplace=True)
-data_2['Customer type'].replace('3', 3, inplace=True)
-data_2['Customer type'].replace('3.0', 3, inplace=True)
-data_2['Customer type'].replace('4', 4, inplace=True)
-data_2['Customer type'].replace('4.0', 4, inplace=True)
-data_2['Customer type'].replace('P', 5, inplace=True)
-data_2['Customer type'].replace('None',np.nan, inplace=True)
+            st.success('You already have all the products.')
+        
+        else:
+            data.rename( columns={'Derivada account':'Derivative account'}, inplace=True )
+            l = list(data.columns)
+            data_desc = pd.read_csv('data_desc.csv')
+            data_desc['Column Name'] = l
+            data_2 = data.copy()
+            data_2 = data_2[data_2['Employee index'].notna()]
+            data_2 = data_2[data_2['Country residence'].notna()]
+            data_2 = data_2[data_2['Sex'].notna()]
+            data_2 = data_2[data_2['Index'].notna()]
+            data_2 = data_2[data_2['Index real'].notna()]
+            data_2 = data_2[data_2['Residence index'].notna()]
+            data_2 = data_2[data_2['Foreigner index'].notna()]
+            data_2 = data_2[data_2['Deceased index'].notna()]
+            data_2 = data_2[data_2['Province code'].notna()]
+            data_2 = data_2[data_2['Province name'].notna()]
+            data_2 = data_2[data_2['Activity index'].notna()]
+            data_2 = data_2[data_2['Payroll'].notna()]
+            data_2 = data_2[data_2['Real pensions'].notna()]
+            data_2.drop(columns=['Last date as primary customer','Spouse index'], inplace=True)
+            data_2['Gross income of the household'] = data_2['Gross income of the household'].fillna(134245.63)
+            data_2['Customer type'] = data_2['Customer type'].fillna(1)
+            data_2['Customer relation type'] = data_2['Customer relation type'].fillna('I')
+            data_2['Channel index'] = data_2['Channel index'].fillna('KHE')
+            data_2['Segmentation index'] = data_2['Segmentation index'].fillna('02 - PARTICULARES')
+            data_2[['Age', 'Seniority (in months)']] = data_2[['Age', 'Seniority (in months)']].astype(int)
+            data_2 = data_2[data_2["Age"] <= 100]
+            data_2 = data_2[data_2["Age"] >= 18]
+            data_2 = data_2[data_2["Seniority (in months)"] != -999999]
+            # Исправление категорий столбца - indrel_1mes
+            data_2['Customer type'].replace('1', 1, inplace=True)
+            data_2['Customer type'].replace('1.0', 1, inplace=True)
+            data_2['Customer type'].replace('2', 2, inplace=True)
+            data_2['Customer type'].replace('2.0', 2, inplace=True)
+            data_2['Customer type'].replace('3', 3, inplace=True)
+            data_2['Customer type'].replace('3.0', 3, inplace=True)
+            data_2['Customer type'].replace('4', 4, inplace=True)
+            data_2['Customer type'].replace('4.0', 4, inplace=True)
+            data_2['Customer type'].replace('P', 5, inplace=True)
+            data_2['Customer type'].replace('None',np.nan, inplace=True)
 
-le = LabelEncoder()
-raw_target = data_2.iloc[:, 22:].idxmax(1)
-transformed_target = le.fit_transform(raw_target)
-data_2['service_opted'] = transformed_target
-data_2['service_opted'] = data_2['service_opted'].astype('uint8')
-names = raw_target.value_counts().index
-values = raw_target.value_counts().values
-names = [data_desc[data_desc['Column Name'] == name]['Description'].values[0] for name in names]
-user_item_matrix = pd.crosstab(index=data_2.Code, columns=le.transform(raw_target), values=1, aggfunc='sum')
-user_item_matrix.fillna(0, inplace=True)
-uim_arr = np.array(user_item_matrix)
-for row,item in tqdm(enumerate(uim_arr)):
-    for column,item_value in enumerate(item):
-        uim_arr[row, column] = uim_arr[row, column] / sum(item)
-user_item_ratio_matrix = pd.DataFrame(uim_arr, columns=user_item_matrix.columns, index=user_item_matrix.index)
-user_item_ratio_stacked = user_item_ratio_matrix.stack().to_frame()
-user_item_ratio_stacked['ncodpers'] = [index[0] for index in user_item_ratio_stacked.index]
-user_item_ratio_stacked['service_opted'] = [index[1] for index in user_item_ratio_stacked.index]
-user_item_ratio_stacked.reset_index(drop=True, inplace=True)
-user_item_ratio_stacked.rename(columns={0:"service_selection_ratio"}, inplace=True)
-user_item_ratio_stacked = user_item_ratio_stacked[['ncodpers','service_opted', 'service_selection_ratio']]
-user_item_ratio_stacked.drop(user_item_ratio_stacked[user_item_ratio_stacked['service_selection_ratio']==0].index, inplace=True)
-user_item_ratio_stacked.reset_index(drop=True, inplace=True)
-reader = Reader(line_format='user item rating', sep=',', rating_scale=(0,1), skip_lines=1)
-data = Dataset.load_from_df(user_item_ratio_stacked, reader=reader)
-trainset = data.build_full_trainset()
-svd = SVD()
-svd_results = cross_validate(algo=svd, data=data, cv=4)
-svd = SVD()
-svd.fit(trainset)
-def get_recommendation(uid,model):    
-    recommendations = [(uid, sid, data_desc[data_desc['Column Name'] == le.inverse_transform([sid])[0]]['Description'].values[0], model.predict(uid,sid).est) for sid in range(24)]
-    recommendations = pd.DataFrame(recommendations, columns=['uid', 'sid', 'service_name', 'pred'])
-    recommendations.sort_values("pred", ascending=False, inplace=True)
-    recommendations.reset_index(drop=True, inplace=True)
-    return recommendations
+            le = LabelEncoder()
+            raw_target = data_2.iloc[:, 22:].idxmax(1)
+            transformed_target = le.fit_transform(raw_target)
+            data_2['service_opted'] = transformed_target
+            data_2['service_opted'] = data_2['service_opted'].astype('uint8')
+            names = raw_target.value_counts().index
+            values = raw_target.value_counts().values
+            names = [data_desc[data_desc['Column Name'] == name]['Description'].values[0] for name in names]
+            user_item_matrix = pd.crosstab(index=data_2.Code, columns=le.transform(raw_target), values=1, aggfunc='sum')
+            user_item_matrix.fillna(0, inplace=True)
+            uim_arr = np.array(user_item_matrix)
+            for row,item in tqdm(enumerate(uim_arr)):
+                for column,item_value in enumerate(item):
+                    uim_arr[row, column] = uim_arr[row, column] / sum(item)
+            user_item_ratio_matrix = pd.DataFrame(uim_arr, columns=user_item_matrix.columns, index=user_item_matrix.index)
+            user_item_ratio_stacked = user_item_ratio_matrix.stack().to_frame()
+            user_item_ratio_stacked['ncodpers'] = [index[0] for index in user_item_ratio_stacked.index]
+            user_item_ratio_stacked['service_opted'] = [index[1] for index in user_item_ratio_stacked.index]
+            user_item_ratio_stacked.reset_index(drop=True, inplace=True)
+            user_item_ratio_stacked.rename(columns={0:"service_selection_ratio"}, inplace=True)
+            user_item_ratio_stacked = user_item_ratio_stacked[['ncodpers','service_opted', 'service_selection_ratio']]
+            user_item_ratio_stacked.drop(user_item_ratio_stacked[user_item_ratio_stacked['service_selection_ratio']==0].index, inplace=True)
+            user_item_ratio_stacked.reset_index(drop=True, inplace=True)
+            reader = Reader(line_format='user item rating', sep=',', rating_scale=(0,1), skip_lines=1)
+            data = Dataset.load_from_df(user_item_ratio_stacked, reader=reader)
+            trainset = data.build_full_trainset()
+            svd = SVD()
+            svd_results = cross_validate(algo=svd, data=data, cv=4)
+            svd = SVD()
+            svd.fit(trainset)
+            def get_recommendation(uid,model):    
+                recommendations = [(uid, sid, data_desc[data_desc['Column Name'] == le.inverse_transform([sid])[0]]['Description'].values[0], model.predict(uid,sid).est) for sid in range(24)]
+                recommendations = pd.DataFrame(recommendations, columns=['uid', 'sid', 'service_name', 'pred'])
+                recommendations.sort_values("pred", ascending=False, inplace=True)
+                recommendations.reset_index(drop=True, inplace=True)
+                return recommendations
 
-st.table(get_recommendation(15890,svd))
-'''
+            st.table(get_recommendation(9999999,svd))
+
