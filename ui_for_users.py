81d2401eaf7b5a7363537723bd24d8bf1c416b18
TatianaKosovskaya <99216721+TatianaKosovskaya@users.noreply.github.com>
2023-05-09 12:43:56 +0300

    Update ui_for_users.py

--- ui_for_users.py
+++ ui_for_users.py
@@ -166,9 +166,31 @@ if click:
             st.subheader('Recommended products:')
             for i in range(len(d)):
                 st.write(str(i + 1), ". ", d[i])
-                '''
+                
                 if d[i] == 'Saving Account':
                     st.caption('A saving account is a type of banking product that allows customers to deposit and withdraw funds, while earning interest on their balance. These accounts typically have lower interest rates compared to other investment options, but they also offer more liquidity and flexibility. Saving accounts can be opened by individuals, as well as joint accounts for couples, families, or businesses. Some saving accounts may require a minimum deposit to open, and may have minimum balance requirements to avoid fees. Overall, saving accounts are a popular and convenient option for customers looking to earn some interest on their savings while still maintaining easy access to their funds.')
+                if d[i] == 'Home Account':
+                    st.caption('A home account is a banking product that is designed to help customers manage their household finances. This type of account can be used for paying bills, depositing paychecks, and setting aside money for home-related expenses such as mortgage payments, property taxes, and maintenance costs. Some home accounts also offer features such as budgeting tools and automatic savings plans to help customers save money and stay on top of their finances. Additionally, many banks offer incentives such as higher interest rates or waived fees for customers who maintain a certain balance in their home account. Overall, a home account is a useful tool for customers who want to keep their home-related finances organized and easily accessible.')
+                if d[i] == 'Pensions':
+                    st.caption('A banking product for customer pensions is designed to help individuals save money for their retirement years. Pensions are long-term savings plans that allow customers to contribute money on a regular basis, with the funds accumulating over time. These products offer various investment options, such as stocks, bonds, and mutual funds, which can help customers grow their savings over the years. Additionally, some pension products may offer tax benefits or employer contributions to help customers save even more. When a customer reaches retirement age, they can begin to withdraw their savings as a source of income in their retirement years.')
+                if d[i] == 'particular Plus Account':
+                    st.caption('The particular plus account is a banking product that offers customers a range of benefits and perks beyond traditional checking and savings accounts. Typically, this account requires a higher minimum balance and may charge a monthly maintenance fee, but in exchange, customers can enjoy features such as premium interest rates, waived fees on certain transactions, access to personalized financial advisors, and exclusive discounts on products and services. This type of account is designed for customers who maintain a higher balance and want to take advantage of additional benefits and services from their bank.')
+                if d[i] == 'particular Account':
+                    st.caption('A particular account is a type of banking product designed for individual customers, also known as personal or retail banking. This account allows customers to carry out daily transactions such as deposits, withdrawals, and transfers, as well as access to various services such as online banking, mobile banking, and ATM facilities. A particular account may also offer additional benefits such as lower fees, higher interest rates, or loyalty rewards. Some banks may require a minimum balance or charge fees for certain transactions or services, while others may offer free checking accounts with no minimum balance or fees. Overall, a particular account is a basic and essential banking product for individual customers to manage their finances and access various banking services.')
+                if d[i] == 'Loans':
+                    st.caption('A banking product for customer loans is a financial service that allows individuals and businesses to borrow money from a bank or other financial institution. Loans can be used for a variety of purposes, such as purchasing a home, financing a car, or starting a business. Banks typically offer a range of loan products with different terms, interest rates, and repayment schedules to meet the needs of different borrowers. Customers must qualify for a loan based on their creditworthiness and ability to repay the loan. Loans are often secured by collateral such as a car or property, or they may be unsecured, meaning they are not backed by any asset.')
+                if d[i] == 'Current Accounts':
+                    st.caption('A current account is a type of bank account that is used for day-to-day transactions. Customers can deposit money into the account and withdraw it when they need it. Current accounts usually come with a debit card that can be used to make purchases or withdraw cash from ATMs. Some banks also offer overdraft facilities on current accounts, allowing customers to borrow money for short periods of time. Current accounts may also come with features such as online banking, mobile banking, and the ability to set up direct debits and standing orders.')
+                if d[i] == 'Medium-term deposits':
+                    st.caption('Medium-term deposits are a type of banking product offered to customers who are looking for a low-risk investment option with a fixed rate of return. With a medium-term deposit account, customers can deposit a lump sum of money for a fixed period, usually between 1 to 5 years, and earn interest on their savings. The interest rate for medium-term deposits is typically higher than that of a regular savings account. Customers can choose to receive interest payments on a monthly, quarterly, or annual basis, or have the interest reinvested into the account. At the end of the term, the original deposit plus interest is returned to the customer. Medium-term deposits offer a safe and reliable way to save money and earn interest over a fixed period.')
+                if d[i] == 'Funds':
+                    st.caption('A banking product for customers is the option to invest in funds. Funds are professionally managed portfolios of investments that typically include a mix of stocks, bonds, and other securities. Customers can choose from a range of funds with different investment objectives, risk levels, and fees. Investing in funds can provide diversification, potentially higher returns, and access to investments that may be difficult to access on an individual basis. However, it is important to note that investing in funds carries risks, and customers should carefully consider their investment goals, risk tolerance, and financial situation before investing.')
+                if d[i] == 'Securities':
+                    st.caption('Banking products related to securities can include brokerage accounts, investment accounts, and trading accounts. These products allow customers to invest in various financial instruments, such as stocks, bonds, mutual funds, exchange-traded funds (ETFs), and options. The bank may also offer research and analysis tools to help customers make informed investment decisions. Securities accounts can be self-directed or managed by a financial advisor, and may involve varying levels of risk depending on the investment strategy chosen. Customers may be charged fees or commissions for trades and account maintenance.')
+                if d[i] == 'Pensions':
+                    st.caption('A banking product for customer pensions is designed to help individuals save money for their retirement years. Pensions are long-term savings plans that allow customers to contribute money on a regular basis, with the funds accumulating over time. These products offer various investment options, such as stocks, bonds, and mutual funds, which can help customers grow their savings over the years. Additionally, some pension products may offer tax benefits or employer contributions to help customers save even more. When a customer reaches retirement age, they can begin to withdraw their savings as a source of income in their retirement years.')
+                if d[i] == 'Credit Card':
+                    st.caption('A credit card is a banking product that allows customers to borrow funds up to a pre-set credit limit to make purchases or withdraw cash. Customers are required to pay back the borrowed funds along with interest charges and other fees within a billing cycle. Credit cards offer various benefits, such as cashback rewards, discounts, and travel perks. They also provide a convenient and secure way to make purchases both online and offline. Credit card issuers may charge annual fees, transaction fees, balance transfer fees, and other charges, so it is essential to read and understand the terms and conditions before applying for a credit card.')
                 if d[i] == 'Saving Account':
                     st.caption('Вид обязательственных отношений, договор, согласно')
                 if d[i] == 'Saving Account':
@@ -193,29 +215,7 @@ if click:
                     st.caption('Вид обязательственных отношений, договор, согласно')
                 if d[i] == 'Saving Account':
                     st.caption('Вид обязательственных отношений, договор, согласно')
-                if d[i] == 'Saving Account':
-                    st.caption('Вид обязательственных отношений, договор, согласно')
-                if d[i] == 'Saving Account':
-                    st.caption('Вид обязательственных отношений, договор, согласно')
-                if d[i] == 'Saving Account':
-                    st.caption('Вид обязательственных отношений, договор, согласно')
-                if d[i] == 'Saving Account':
-                    st.caption('Вид обязательственных отношений, договор, согласно')
-                if d[i] == 'Saving Account':
-                    st.caption('Вид обязательственных отношений, договор, согласно')
-                if d[i] == 'Saving Account':
-                    st.caption('Вид обязательственных отношений, договор, согласно')
-                if d[i] == 'Saving Account':
-                    st.caption('Вид обязательственных отношений, договор, согласно')
-                if d[i] == 'Saving Account':
-                    st.caption('Вид обязательственных отношений, договор, согласно')
-                if d[i] == 'Saving Account':
-                    st.caption('Вид обязательственных отношений, договор, согласно')
-                if d[i] == 'Saving Account':
-                    st.caption('Вид обязательственных отношений, договор, согласно')
-                if d[i] == 'Saving Account':
-                    st.caption('Вид обязательственных отношений, договор, согласно')
-                '''
+                
                     
             #st.table(get_recommendation(9999999,svd))
 
