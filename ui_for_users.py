f42df48942cf6dfcb313f035bdbbed2e46b1f5f1
TatianaKosovskaya <99216721+TatianaKosovskaya@users.noreply.github.com>
2023-05-10 12:40:06 +0300

    Update ui_for_users.py

--- ui_for_users.py
+++ ui_for_users.py
@@ -36,19 +36,19 @@ from surprise import accuracy
 from surprise.prediction_algorithms.knns import KNNBasic
 
 data_1 = pd.read_csv('train_df_1.csv')
-data_2 = pd.read_csv('train_df_2.csv')
-data_3 = pd.read_csv('train_df_3.csv')
-data_4 = pd.read_csv('train_df_4.csv')
-data_5 = pd.read_csv('train_df_5.csv')
-data_6 = pd.read_csv('train_df_6.csv')
-data_7 = pd.read_csv('train_df_7.csv')
-data_8 = pd.read_csv('train_df_8.csv')
+#data_2 = pd.read_csv('train_df_2.csv')
+#data_3 = pd.read_csv('train_df_3.csv')
+#data_4 = pd.read_csv('train_df_4.csv')
+#data_5 = pd.read_csv('train_df_5.csv')
+#data_6 = pd.read_csv('train_df_6.csv')
+#data_7 = pd.read_csv('train_df_7.csv')
+#data_8 = pd.read_csv('train_df_8.csv')
 #data_9 = pd.read_csv('train_df_9.csv')
 #data_10 = pd.read_csv('train_df_10.csv')
 #data = pd.concat([data_1, data_2, data_3, data_4, data_5, data_6, data_7, data_8, data_9, data_10], axis=0, ignore_index=True)
-data = pd.concat([data_1, data_2, data_3, data_4, data_5, data_6, data_7, data_8], axis=0, ignore_index=True)
+#data = pd.concat([data_1, data_2, data_3, data_4, data_5, data_6, data_7, data_8], axis=0, ignore_index=True)
 
-#data=data_1
+data=data_1
 data.drop('Unnamed: 0', axis= 1 , inplace= True )
 
 col1, col2 = st.columns(2)
